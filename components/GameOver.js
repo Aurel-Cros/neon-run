f2f080b1a6aeda78c23b63e4176bac60b38f58d4
Salomé <s.vauchier@onlineformapro.com>
2023-07-07 16:32:23 +0200

    feat/component/GameOver - js/css - GameOverOverlay

---
+++ components/GameOver.js
@@ -0,0 +1,46 @@
+export class GameOver {
+  constructor() {
+    this._makeOverlay();
+    this._initListeners();
+  }
+
+  _makeOverlay() {
+    const gameOverWrapper = document.createElement("div");
+    gameOverWrapper.id = "gameOverWrapper";
+    document.querySelector(".game-wrapper").appendChild(gameOverWrapper);
+
+    const gameOverText = document.createElement("h1");
+    gameOverText.id = "gameOverText";
+    gameOverText.innerHTML = "GAME OVER";
+    gameOverWrapper.appendChild(gameOverText);
+
+    const gameOverTextShadow = document.createElement("p");
+    gameOverTextShadow.id = "gameOverTextShadow";
+    gameOverTextShadow.innerHTML = "GAME OVER";
+    gameOverWrapper.appendChild(gameOverTextShadow);
+
+    const buttonWrapper = document.createElement("div");
+    buttonWrapper.classList.add("buttonWrapper");
+    gameOverWrapper.appendChild(buttonWrapper);
+
+    const retryButton = document.createElement("button");
+    retryButton.id = "retryButton";
+    retryButton.innerHTML = "Retry";
+    buttonWrapper.appendChild(retryButton);
+
+    const quitButton = document.createElement("button");
+    quitButton.id = "quitButton";
+    quitButton.innerHTML = "Quit";
+    buttonWrapper.appendChild(quitButton);
+  }
+  _initListeners() {
+    const retryButton = document.getElementById("retryButton");
+    retryButton.addEventListener("click", () => {
+      document.body.replaceChildren();
+      startGame();
+    });
+
+    const quitButton = document.getElementById("quitButton");
+    quitButton.addEventListener("click", () => {});
+  }
+}
